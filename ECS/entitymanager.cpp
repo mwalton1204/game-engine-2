bc8ad945c59605bcb343d5dffba0c469a340a645
Michael <michael.walton@g.austincc.edu>
2024-01-28 13:16:03 -0600

    implementation of constructor, createEntity, getQtyEntities

--- ECS/entitymanager.cpp
+++ ECS/entitymanager.cpp
@@ -1,1 +1,24 @@
 #include "entitymanager.h"
+
+EntityManager::EntityManager() {
+    for(Entity i = 0; i < MAX_ENTITIES; i++) {
+        availableIDs.push(i);
+    }
+}
+
+Entity EntityManager::createEntity() {
+    Entity newEntity = availableIDs.front();
+    availableIDs.pop();
+
+    ComponentSet newComponentSet;
+
+    livingEntities[newEntity] = newComponentSet;
+
+    return newEntity;
+}
+
+void EntityManager::destroyEntity(Entity id) {}
+
+int EntityManager::getQtyEntities() {
+    return livingEntities.size();
+}
