cf31142b60ba9dcaf256acb750375ddabb1fbf18
Michael <michael.walton@g.austincc.edu>
2024-01-28 12:58:21 -0600

    EntityManager class prototype

--- ECS/entitymanager.h
+++ ECS/entitymanager.h
@@ -1,5 +1,23 @@
 #pragma once
 
+#include <unordered_map>
+
+#include "constants.h"
+
 class EntityManager {
-    
+
+    public:
+
+        EntityManager();
+        Entity createEntity();
+        void destroyEntity(Entity);
+        int getQtyEntities();
+        ComponentSet& getComponentSet(Entity);
+
+
+    private:
+
+        std::unordered_map<Entity, ComponentSet> livingEntities;
+
+
 };
