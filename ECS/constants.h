bce7f2f2eceb10eebeab389ef1c062d3849a395d
Michael <michael.walton@g.austincc.edu>
2024-01-28 12:19:46 -0600

    set constants

--- ECS/constants.h
+++ ECS/constants.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <cstdint>
+#include <bitset>
+
+using Entity = uint32_t;
+const Entity MAX_ENTITIES = 50;
+
+using Component = uint8_t;
+const Component MAX_COMPONENTS = 10;
+
+using ComponentFlags = std::bitset<MAX_COMPONENTS>;
