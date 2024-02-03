ef943044eb13acb2cae757506736cfee406ef234
Michael <michael.walton@g.austincc.edu>
2024-02-03 13:43:34 -0600

    binary arithmentic operator overload implementation

---
+++ Math/vec2.cpp
@@ -0,0 +1,40 @@
+#include "vec2.h"
+
+Vec2::Vec2() : x(0.0f), y(0.0f) {}
+Vec2::Vec2(float x, float y) : x(x), y(y) {}
+
+Vec2 Vec2::operator+(const Vec2& other) const {
+    return Vec2(x + other.x, y + other.y);
+}
+
+Vec2 Vec2::operator-(const Vec2& other) const {
+    return Vec2(x - other.x, y - other.y);
+}
+
+Vec2 Vec2::operator*(float scalar) const {
+    return Vec2(x * scalar, y * scalar);
+}
+
+Vec2 Vec2::operator*(const Vec2& other) const {
+    return Vec2(x * other.x, y * other.y);
+}
+
+Vec2 Vec2::operator/(float scalar) const {
+    Vec2 scaledVec;
+    if(scalar != 0) {
+        scaledVec.x = x / scalar;
+        scaledVec.y = y / scalar;
+    }
+
+    return scaledVec;
+}
+
+Vec2 Vec2::operator/(const Vec2& other) const {
+    Vec2 scaledVec;
+    if(other.x != 0 && other.y != 0) {
+        scaledVec.x = x / other.x;
+        scaledVec.y = y / other.y;
+    }
+
+    return scaledVec;
+}
