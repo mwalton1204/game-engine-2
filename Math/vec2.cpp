a52495b6a6a6f1ecc36c53ff15614646770688db
Michael <michael.walton@g.austincc.edu>
2024-02-03 15:38:57 -0600

    relational operator overloads implementation

--- Math/vec2.cpp
+++ Math/vec2.cpp
@@ -119,3 +119,11 @@ Vec2& Vec2::operator=(const Vec2& other) {
 
     return *this;
 }
+
+bool Vec2::operator==(const Vec2& other) const {
+    return x == other.x && y == other.y;
+}
+
+bool Vec2::operator!=(const Vec2& other) const {
+    return x != other.x || y != other.y;
+}
