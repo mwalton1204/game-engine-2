9cb264eed0b6bf5418a5c86f9d869e4ca80224d2
Michael <michael.walton@g.austincc.edu>
2024-02-03 16:25:20 -0600

    switched from struct to class. prototyped several utility methods

--- Math/vec2.h
+++ Math/vec2.h
@@ -1,37 +1,48 @@
 #pragma once
 
-struct Vec2 {
+class Vec2 {
 
-    Vec2();
-    Vec2(float, float);
+    public:
+        float x, y, z;
+
+        Vec2();
+        Vec2(float, float);
 
 // Binary Arithmetic
-    Vec2 operator+(const Vec2&) const;
-    Vec2 operator-(const Vec2&) const;
-    Vec2 operator*(const float) const;
-    Vec2 operator*(const Vec2&) const;
-    Vec2 operator/(const float) const;
-    Vec2 operator/(const Vec2&) const;
+        Vec2 operator+(const Vec2&) const;
+        Vec2 operator-(const Vec2&) const;
+        Vec2 operator*(const float) const;
+        Vec2 operator*(const Vec2&) const;
+        Vec2 operator/(const float) const;
+        Vec2 operator/(const Vec2&) const;
 
 //Unary Arithmetic
-    Vec2& operator++();
-    Vec2 operator++(int);
-    Vec2& operator--();
-    Vec2 operator--(int);
+        Vec2& operator++();
+        Vec2 operator++(int);
+        Vec2& operator--();
+        Vec2 operator--(int);
 
 //Assignment
-    Vec2& operator+=(const Vec2&);
-    Vec2& operator-=(const Vec2&);
-    Vec2& operator*=(const float);
-    Vec2& operator*=(const Vec2&);
-    Vec2& operator/=(const float);
-    Vec2& operator/=(const Vec2&);
-    Vec2& operator=(const Vec2&);
+        Vec2& operator+=(const Vec2&);
+        Vec2& operator-=(const Vec2&);
+        Vec2& operator*=(const float);
+        Vec2& operator*=(const Vec2&);
+        Vec2& operator/=(const float);
+        Vec2& operator/=(const Vec2&);
+        Vec2& operator=(const Vec2&);
 
 //Relational
-    bool operator==(const Vec2&) const;
-    bool operator!=(const Vec2&) const;
+        bool operator==(const Vec2&) const;
+        bool operator!=(const Vec2&) const;
 
-    float x, y, z;
+//Utility Methods
 
+        //magnitude
+        float magnitude () const;
+        //normalize
+        void normalize();
+        //dot
+        float dot(const Vec2&) const;
+        //angle
+        float angle(const Vec2&) const;
 };
